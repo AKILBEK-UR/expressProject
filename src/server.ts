f35d49f309f8c5970e06b1149c8eb0e90e8bd22e
Akilbek <akilbekurinbaev0616@gmail.com>
2024-10-03 06:09:26 +0500

    added prettier

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+import express from 'express';
+import dotenv from 'dotenv';
 
-const dbHost = process.env.DB_HOST; 
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
